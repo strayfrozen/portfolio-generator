67d316a5de93fc93a95863ea2a8ce30d208db9b6
Stray <strayfrozen@yahoo.com>
2021-12-02 17:59:20 -0600

    saving todays progress

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
-/*const fs = require('fs');
+const fs = require('fs');
 const generatePage = require('./src/page-template');
-
+/*
 const pageHTML = generatePage(name, github);
 
 fs.writeFile('./index.html', pageHTML, err => {
@@ -137,5 +137,9 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    // const pageHTML = generatePage();
+
+    // fs.writeFile('./index.html', pageHTML, err => {
+    //   if (err) throw new Error(err);
+    // });
   });
