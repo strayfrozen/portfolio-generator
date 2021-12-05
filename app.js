881aed395899ef6448d9735cf3f659b1e700d8c0
Stray <strayfrozen@yahoo.com>
2021-12-04 18:22:48 -0600

    finished project fixed bugs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
 const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generate-site');
+
 
 const promptUser = () => {
   return inquirer.prompt([
